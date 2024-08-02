38c6cd362b6b5abb40286fbda357dc1336f51afa
HebaRouk <113283778+HebaRouk@users.noreply.github.com>
2024-08-02 09:00:23 +0400

    Update train.py

--- My Proj2/Create-Your-Own-Image-Classifier-main/train.py
+++ My Proj2/Create-Your-Own-Image-Classifier-main/train.py
@@ -1,13 +1,5 @@
-
-# Basic usage: python train.py data_directory
-# Prints out training loss, validation loss, and validation accuracy as the network trains
-# Options:
-#         Set directory to save checkpoints: python train.py data_dir --save_dir save_directory
-#         Choose architecture: python train.py data_dir --arch "vgg13"
-#         Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
-#         Use GPU for training: python train.py data_dir --gpu
-
-# sample bash cmd: python train.py './flowers' '../saved_models' --epochs 5
+# Heba Rouk
+# 2/8/2024
 
 import argparse
 import torch
@@ -239,5 +231,5 @@ if __name__ == '__main__':
 
     # train and save model
     train_model(args, train_data_loader, valid_data_loader, class_to_idx)
-    
+    ###################################################################
     
